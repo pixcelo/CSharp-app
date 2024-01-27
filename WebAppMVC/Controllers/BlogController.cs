f4c559a3ef6078f6aa7b1c0275dc1b734f52e13f
pixcelo <bodomu@gmail.com>
2024-01-27 10:29:52 +0900

    add: 画面に表示、画面遷移の追加

---
+++ WebAppMVC/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly BlogContext _context;
+
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var blogs = _context.Blog.ToList();
+            return View(blogs);
+        }
+    }
+}
