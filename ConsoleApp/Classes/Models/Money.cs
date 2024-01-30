81116dabd24c12ebe5e701f44dce393c78cd2cbf
pixcelo <bodomu@gmail.com>
2024-01-31 03:49:44 +0900

    add: Moneyクラス

---
+++ ConsoleApp/Classes/Models/Money.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp.Classes.Models
+{
+    public class Money
+    {
+        private readonly decimal amount;
+        private readonly decimal currency;
+
+        public Money(decimal amount, decimal currency)
+        {
+            if (currency == 0)
+            {
+                throw new ArgumentNullException(nameof(currency));
+            }
+
+            this.amount = amount;
+            this.currency = currency;
+        }
+    }
+}
