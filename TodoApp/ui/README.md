0d30f1bdd6c4fb005204cf3cec5086b215c0d4a7
pixcelo <bodomu@gmail.com>
2024-02-05 05:17:53 +0900

    add: todoApp ui

--- TodoApp/ui/README.md
+++ TodoApp/ui/README.md
@@ -1,5 +1,14 @@
 # React ui
+
 実行コマンド
 ```
+docker-compose up --build
+docker-compose exec todoapp sh
+```
+
+コンテナ
+```
 npx create-react-app todoapp
+cd todoapp
+npm start
 ```
--- TodoApp/ui/app/todoapp/src/App.js
+++ TodoApp/ui/app/todoapp/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes:[]
+    }
+  }
+
+  API_URL = "http://localhost:5297/";
+
+  componentDidMount() {
+    this.refreshNotes();
+  }
+
+  async refreshNotes() {
+    fetch(this.API_URL + "api/TodoApp/GetNotes")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ notes: data })
+    })
+    .catch(error => {
+      console.error("Error :", error);
+    });
+  }
+
+  async addClick() {
+    const newNotes = document.getElementById("newNotes").value;
+    const data = new FormData();
+    data.append("newNotes", newNotes);
+
+    fetch(this.API_URL + "api/TodoApp/AddNotes", {
+      method: "POST",
+      body: data
+    }).then( response => response.json())
+    .then((result) => {
+      alert(result);
+      this.refreshNotes();
+    })
+    .catch(error => {
+      console.error("Error :", error);
+    });
+  }
+
+  async deleteClick(id) {    
+    fetch(this.API_URL + "api/TodoApp/DeleteNotes?id="+id, {
+      method: "DELETE",      
+    }).then( response => response.json())
+    .then((result) => {
+      alert(result);
+      this.refreshNotes();
+    })
+    .catch(error => {
+      console.error("Error :", error);
+    });
+  }
+
+  render() {
+    const { notes } = this.state;
+    return (
+      <div className="App">
+        <h2>Todo App</h2>
+          <input id="newNotes" />&nbsp;
+          <button onClick={() => this.addClick()}>Add Notes</button>
+        {notes.map(note =>
+          <p key={note.id}>
+            <b>* {note.description}</b>&nbsp;
+            <button onClick={() => this.deleteClick(note.id)}>Delete Notes</button>
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
