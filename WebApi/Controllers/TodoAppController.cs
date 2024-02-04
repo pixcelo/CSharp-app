1006f52a5d8af4fa85ddb76b43dd88f27f11fbbd
pixcelo <bodomu@gmail.com>
2024-02-04 14:28:10 +0900

    add: Todo API

---
+++ WebApi/Controllers/TodoAppController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public TodoAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes()
+        {
+            string query = "select * from dbo.notes";
+            var table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDBCon");
+            SqlDataReader reader;
+            using (var con = new SqlConnection(sqlDatasource))
+            {
+                con.Open();
+                using (var command = new SqlCommand(query, con))
+                {
+                    reader = command.ExecuteReader();
+                    table.Load(reader);
+                    reader.Close();
+                    con.Close();
+                }
+
+                return new JsonResult(table);
+            }
+        }
+    }
+}
