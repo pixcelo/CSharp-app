bdb7cc57284397546b0ce35935d19140cb958ebb
pixcelo <bodomu@gmail.com>
2024-02-06 07:44:02 +0900

    add: Stripe決済システム

---
+++ WebAppMvcStripe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppMvcStripe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebAppMvcStripe/Models/StripModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppMvcStripe.Models
+{
+    public class StripModel
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public long Rate { get; set; }
+        public long Quanity { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
