8f0c9abd15a06b1d55fbc17cb5e246d4bf33763a
pixcelo <bodomu@gmail.com>
2024-02-07 03:28:37 +0900

    add: Controller

---
+++ WebAppMvcStripe/Controllers/CheckOutController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMvcStripe.Models;
+
+namespace WebAppMvcStripe.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<ProductEntity>();
+
+            products = new List<ProductEntity>
+            {
+                new ProductEntity
+                {
+                    Product = "Test Name",
+                    Rate = 1500,
+                    Quanity = 2,
+                    ImagePath = "img/Image1.png"
+                },
+                new ProductEntity
+                {
+                    Product = "Test Name2",
+                    Rate = 1000,
+                    Quanity = 3,
+                    ImagePath = "img/Image2.png"
+                }
+            };
+
+            return View(products);
+        }
+    }
+}
