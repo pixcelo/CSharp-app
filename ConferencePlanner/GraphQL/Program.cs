62eaaa5cc6ced7035a5d0f46222525429eeeaa23
pixcelo <bodomu@gmail.com>
2024-02-21 04:44:02 +0900

    GraphQLサーバー起動まで確認

---
+++ ConferencePlanner/GraphQL/Program.cs
@@ -0,0 +1,28 @@
+using ConferencePlanner.GraphQL;
+using ConferencePlanner.GraphQL.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// サービスの登録
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlite("Data Source=conferences.db"));
+
+builder.Services
+	.AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+
+// ルーティングのミドルウェアを追加
+app.UseRouting();
+
+// エンドポイントのミドルウェアを設定
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGraphQL();
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
