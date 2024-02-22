46168ce3db1e8959ccfc3a08a4636b71c2f43153
pixcelo <bodomu@gmail.com>
2024-02-23 08:30:10 +0900

    add: Excel画像抽出アドイン

---
+++ ExcelAddIn/RibbonImage.cs
@@ -0,0 +1,50 @@
+﻿using ExcelAddIn.Services;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Windows.Forms;
+
+namespace ExcelAddIn
+{
+	public partial class RibbonImage
+	{
+		private void RibbonImage_Load(object sender, RibbonUIEventArgs e)
+		{
+
+		}
+
+		private void buttonImage_Click(object sender, RibbonControlEventArgs e)
+		{
+			try
+			{
+				var excelApp = Globals.ThisAddIn.Application;
+				var workbook = excelApp.ActiveWorkbook;
+				if (workbook != null)
+				{
+					string workbookPath = workbook.FullName;
+					var imageService = new ImageService();
+					bool result = imageService.SaveImagesFromWorkbook(workbookPath);
+					if (result)
+					{
+						MessageBox.Show(
+							"画像が正常に保存されました。",
+							"成功",
+							MessageBoxButtons.OK,
+							MessageBoxIcon.Information);
+					}
+					else
+					{
+						MessageBox.Show(
+							"保存する画像が見つかりませんでした。",
+							"警告",
+							MessageBoxButtons.OK,
+							MessageBoxIcon.Warning);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"エラー: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
---
+++ ExcelAddIn/Services/ImageService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using Workbook = Microsoft.Office.Interop.Excel.Workbook;
+using Worksheet = Microsoft.Office.Interop.Excel.Worksheet;
+
+namespace ExcelAddIn.Services
+{
+	public class ImageService
+	{
+		public bool SaveImagesFromWorkbook(string workbookPath)
+		{
+			Application excelApp = null;
+			Workbook workbook = null;
+
+			try
+			{
+				excelApp = new Application();
+				workbook = excelApp.Workbooks.Open(workbookPath);
+
+				string saveDirectory = Path.Combine(
+					Path.GetDirectoryName(workbookPath),
+					Path.GetFileNameWithoutExtension(workbookPath) + "_Images");
+
+				if (!Directory.Exists(saveDirectory))
+				{
+					Directory.CreateDirectory(saveDirectory);
+				}
+
+				int imageIndex = 0;
+				foreach (Worksheet ws in workbook.Sheets)
+				{
+					foreach (Shape shape in ws.Shapes)
+					{
+						if (shape.Type == Microsoft.Office.Core.MsoShapeType.msoPicture)
+						{
+							string imagePath = Path.Combine(saveDirectory, $"Image_{++imageIndex}.jpg");
+							shape.CopyPicture(XlPictureAppearance.xlScreen, XlCopyPictureFormat.xlBitmap);
+
+							if (Clipboard.ContainsImage())
+							{
+								var image = Clipboard.GetImage();
+								image.Save(imagePath, ImageFormat.Png);
+							}
+						}
+					}
+				}
+
+				return imageIndex > 0;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Error: {ex.Message}");
+				return false;
+			}
+			finally
+			{
+				if (workbook != null)
+				{
+					workbook.Close(false);
+					Marshal.ReleaseComObject(workbook);
+				}
+				if (excelApp != null)
+				{
+					excelApp.Quit();
+					Marshal.ReleaseComObject(excelApp);
+				}
+			}
+		}
+
+	}
+}
