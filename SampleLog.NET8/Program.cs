5e461efab014b629366c7c66febd2a90486ec5cf
pixcelo <bodomu@gmail.com>
2024-03-28 07:34:59 +0900

    update: デバッグ時にコンソールに出力する

--- SampleLog.NET8/Program.cs
+++ SampleLog.NET8/Program.cs
@@ -1,13 +1,22 @@
+using System.Runtime.InteropServices;
+
 namespace SampleLog.NET8
 {
     internal static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+#if DEBUG
+            AllocConsole();
+#endif
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
