e21ecda24d80943b74e3063b5023f7b6570b99ce
pixcelo <bodomu@gmail.com>
2024-03-29 05:07:12 +0900

    update: App.configをリリースとデバッグで分割

--- SampleLog.NET8/AssemblyInfo.cs
+++ SampleLog.NET8/AssemblyInfo.cs
@@ -1,1 +1,5 @@
-﻿[assembly: log4net.Config.XmlConfigurator(Watch=true)]
+﻿#if DEBUG
+	[assembly: log4net.Config.XmlConfigurator(Watch = true, ConfigFile = "Config/App.Debug.config")]
+#else
+	[assembly: log4net.Config.XmlConfigurator(Watch = true, ConfigFile = "Config/App.Release.config")]
+#endif
