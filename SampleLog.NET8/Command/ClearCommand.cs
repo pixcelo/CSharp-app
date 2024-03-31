1fd81b9ddcaeff6d3c14510ec56fd2ad00de421e
pixcelo <bodomu@gmail.com>
2024-03-31 16:00:14 +0900

    add: クリアボタンのロジック追加

---
+++ SampleLog.NET8/Command/ClearCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace SampleLog.NET8.Command
+{
+    public class ClearCommand : ICommand
+    {
+        private readonly CalculatorForm _form;        
+        private string _previousValue;
+
+        public ClearCommand(CalculatorForm form)
+        {
+            _form = form;            
+        }
+
+        public void Invoke()
+        {
+            var currentValue = _form.GetTextBoxDisplay();
+            _previousValue = currentValue;
+
+            _form.SetTextBoxDisplay("");
+        }
+
+        public void Undo()
+        {
+            _form.SetTextBoxDisplay(_previousValue);
+        }
+
+        public void Redo()
+        {
+            Invoke();
+        }
+    }
+}
