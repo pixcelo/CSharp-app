e18c5ec90c8fe67731784654b7850e3bfcf55170
pixcelo <bodomu@gmail.com>
2024-04-03 05:42:48 +0900

    update : エントリーポイント修正

--- SampleLog.NET8/Program.cs
+++ SampleLog.NET8/Program.cs
@@ -8,19 +8,14 @@ namespace SampleLog.NET8
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool AllocConsole();
 
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
 #if DEBUG
             AllocConsole();
 #endif
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new CalculatorForm());
         }
     }
 }
