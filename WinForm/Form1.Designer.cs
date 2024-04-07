14534a1e35eff21c75f99b7b91aaf206f9a814c1
pixcelo <bodomu@gmail.com>
2024-04-07 17:34:14 +0900

    add: QRコード生成の機能

--- WinForm/Form1.Designer.cs
+++ WinForm/Form1.Designer.cs
@@ -28,34 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnQR = new System.Windows.Forms.Button();
+            this.pictureBoxQR = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnQR
             // 
-            this.button1.Location = new System.Drawing.Point(321, 149);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 95);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnQR.Location = new System.Drawing.Point(487, 62);
+            this.btnQR.Name = "btnQR";
+            this.btnQR.Size = new System.Drawing.Size(231, 95);
+            this.btnQR.TabIndex = 0;
+            this.btnQR.Text = "QRコード作成";
+            this.btnQR.UseVisualStyleBackColor = true;
+            this.btnQR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBoxQR
+            // 
+            this.pictureBoxQR.Location = new System.Drawing.Point(53, 62);
+            this.pictureBoxQR.Name = "pictureBoxQR";
+            this.pictureBoxQR.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxQR.TabIndex = 1;
+            this.pictureBoxQR.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(925, 541);
+            this.Controls.Add(this.pictureBoxQR);
+            this.Controls.Add(this.btnQR);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnQR;
+        private System.Windows.Forms.PictureBox pictureBoxQR;
     }
 }
 
---
+++ WinForm/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Web;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
+
+namespace SampleLog
+{
+    public partial class Form1 : Form
+    {
+        private static log4net.ILog logger =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string URL = "https://qiita.com";
+            var bitmap = GenerateQRcode(URL);
+
+            pictureBoxQR.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBoxQR.Image = bitmap;
+        }
+
+        /// <summary>
+        /// QRコード生成
+        /// </summary>
+        /// <param name="URL"></param>
+        /// <returns></returns>
+        private Bitmap GenerateQRcode(string URL)
+        {
+            var writer = new BarcodeWriter
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = 200,
+                    Width = 200,
+                    Margin = 1
+                }
+            };
+
+            return writer.Write(URL);
+        }
+    }
+}
