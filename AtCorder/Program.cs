d753c092f42f634389b2dbabef2e4faf9543d989
pixcelo <bodomu@gmail.com>
2024-04-18 04:38:33 +0900

    標準入力の確認

--- AtCorder/Program.cs
+++ AtCorder/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+/*
+ 問題文
+ 高橋君はデータの加工が行いたいです。
+ 整数 a, b, cと、文字列 s が与えられます。 
+ a+b+c の計算結果と、文字列 s を並べて表示しなさい。
+
+ 制約
+ 1≤a, b, c≤1,000
+ 1≤∣s∣≤100
+
+ 入力
+ 入力は以下の形式で与えられる。
+ a
+ b c
+ s
+*/
+
+
+int a = int.Parse(Console.ReadLine());
+int[] bc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+string s = Console.ReadLine();
+Console.WriteLine(($"{a + bc[0] + bc[1]} {s}"));
