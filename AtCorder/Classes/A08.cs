88addfbdf25bff0137c78ed3a5a41e4ab09905e3
pixcelo <bodomu@gmail.com>
2024-04-30 05:24:11 +0900

    add: A08 問題を追加

---
+++ AtCorder/Classes/A08.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;　
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AtCorder.Classes
+{
+    internal class A08 // Two Dimensional Sum
+    {
+        /*
+        https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_h
+
+        問題文
+        H × W のマス目があります．上からi行目，左からj列目にあるマス (i,j) には，整数Xi,j​が書かれています． 
+        これについて，以下の Q 個の質問に答えるプログラムを作成してください．
+            ・i 個目の質問：左上 (Ai,Bi) 右下 (Ci,Di) の長方形領域に書かれた整数の総和は？
+
+        制約
+        1≤H,W≤1500
+        1≤Q≤100000
+        0≤Xi,j≤9
+        1≤Ai≤Ci≤H
+        1≤Bi≤Di≤W
+        入力はすべて整数
+
+        入力
+        入力は以下の形式で標準入力から与えられます．
+        H W
+        X1,1 X1,2 ⋯ X1,W
+        ⋮
+        XH,1 XH,2 ⋯ XH,W
+        Q
+        A1 B1 C1 D1
+        ⋮
+        AQ BQ CQ DQ
+​
+        出力
+        Q 行にわたって出力してください．i行目には，質問iの答えを出力してください．
+
+        入力例
+        5 5
+        2 0 0 5 1
+        1 0 3 0 0
+        0 8 5 0 2
+        4 1 0 0 6
+        0 9 2 7 0
+        2
+        2 2 4 5
+        1 1 5 5
+
+        出力例
+        25
+        56
+
+        */
+
+        public void Run()
+        {
+            while (true)
+            {
+                
+
+            }
+        }
+        
+    }
+}
--- AtCorder/Program.cs
+++ AtCorder/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var p = new A07();
+        var p = new A08();
         p.Run();
     }
 }
