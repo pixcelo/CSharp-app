46b9dbf1832bc3b7aef358238131031e8b018958
pixcelo <bodomu@gmail.com>
2024-05-06 04:46:38 +0900

    add: 基礎、Tips確認

---
+++ LinkedinLearning/ConsoleDotNetFramework/Basis/DictionaryTips.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNetFramework.Basis
+{
+    public class Dictionary01
+    {
+        public void Run()
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("key1", "value1");
+            dict.Add("key2", "value2");
+            dict.Add("key3", "value3");
+
+            /*
+                ContainsKeyでの確認は、通常では問題がないが
+                別のスレッドによってキーが削除される可能性は残る
+            */
+
+            string result = "";
+
+            if (dict.ContainsKey("key2"))
+            {
+                result = dict["key2"];
+            }
+
+            // こちらの書き方なら、値が取得できた場合のみ実行可能となる
+            if (dict.TryGetValue("key2", out result))
+            {
+                // 値が取得できた場合の処理
+                Console.WriteLine(result);
+            }
+            else
+            {
+                // 値が取得できなかった場合の処理
+                Console.WriteLine("値が取得できませんでした。");
+            }
+        }        
+    }
+}
---
+++ LinkedinLearning/ConsoleDotNetFramework/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleDotNetFramework.Basis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNetFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // コンソールアプリは Ctr + F5 で起動
+            Console.WriteLine("Hello World");
+            Console.WriteLine("===========");
+
+            var model = new DictionaryTips();
+            model.Run();
+
+            // 動的な変数の宣言 
+            //dynamic dynamicValue = 3;
+            //dynamicValue = "abc";
+            //dynamicValue.SomeMethod();
+
+            // 複数の引数を渡す
+            //int Sum(params int[] a)
+            //{
+            //    return a[0] + a[1];
+            //}
+        }
+    }
+}
