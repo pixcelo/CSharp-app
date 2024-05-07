9a219f8e10cae1ac9c56fb767549cf5b9e4e11b8
pixcelo <bodomu@gmail.com>
2024-05-07 09:29:02 +0900

    add: ジェネリック

--- LinkedinLearning/ConsoleDotNetCore/Generic/GeClass.cs
+++ LinkedinLearning/ConsoleDotNetCore/Generic/GeClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,59 @@ using System.Threading.Tasks;
 namespace ConsoleDotNetCore.Generic
 {
     /// <summary>
-    /// ジェネリックのぷｂクラス
+    /// ジェネリック型クラス
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class GeClass<T>
     {
         public T value;
+
+        public void SomeMethod(T arg)
+        {
+            Console.WriteLine(arg);
+        }
+
+        public static void GeMethod<T2>(T2 arg)
+        {
+
+        }
     }
+
+    public class GeClass<T, T2>
+    {
+        public T value;
+        public T2 value2;        
+    }
+
+    /// <summary>
+    /// ジェネリック型クラスを継承する場合は型を指定する必要あり
+    /// </summary>
+    public class AClass : GeClass<int>{　}
+    public class BClass<T> : GeClass<T>{　}
+
+    public class CClass
+    {
+        // whereで制約を付けられる（下記は型パラメーターがstruct（値型）であることを制約とする）
+        public void SomeMethod<T>(T arg)
+            where T : struct
+        {
+            Console.WriteLine(arg);
+        }
+
+        public void SomeMethod2<T>(T arg)
+            where T : class // class(参照型）に制約を指定する
+        {
+            Console.WriteLine(arg);
+        }
+
+        // 複数の型制約を記述可能
+        public void SomeMethod3<T, T2>(T arg)
+            where T : class, IDisposable, new()
+            where T2 : class
+        {
+            Console.WriteLine(arg);
+        }
+
+    }
+
 }
--- LinkedinLearning/ConsoleDotNetCore/Generic/GenericTips.cs
+++ LinkedinLearning/ConsoleDotNetCore/Generic/GenericTips.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,37 @@ namespace ConsoleDotNetCore.Generic
             geString.value = "aaa";
 
             Console.WriteLine(geString.value);
+
+            // Func型
+            Func<int, int> func = SomeMethod;
+
+            // ジェネリックなクラスにリフレクションを使用する
+            Type type = typeof(GeClass<int>);
+
+            // ジェネリックかどうかを判定
+            if (type.IsGenericType)
+            {
+                //Console.WriteLine("is Generic");
+
+                // GetMethodにはメソッドの名前を渡す
+                MethodInfo method = type.GetMethod("GeMethod");
+
+                // ジェネリックなメソッドかどうかを判定
+                if (method.IsGenericMethod) // methodがnullだとnullExceptionが発生
+                {
+                    // ジェネリックなメソッドの型パラメーターを指定
+                    MethodInfo geMethod = method.MakeGenericMethod(typeof(int));
+
+                    // メソッドを呼び出す
+                    geMethod.Invoke(null, new object[] { 3 });
+                }
+            }
+        }      
+
+        public int SomeMethod(int i)
+        {
+            return i * 2;
         }
+
     }
 }
