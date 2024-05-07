9104ad42f96f40434ccddbaa0c04ab27757b8685
pixcelo <bodomu@gmail.com>
2024-05-07 10:09:46 +0900

    add: .NET Frameworkのテストプロジェクト

---
+++ LinkedinLearning/ConsoleDotNetFramework/UnitTest/Calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNetFramework.UnitTest
+{
+    /// <summary>
+    /// ユニットテスト対象クラス
+    /// </summary>
+    public class Calc
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
