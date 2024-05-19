03bbd36f136c755c9fd7d7d81a61dd3828cde657
pixcelo <bodomu@gmail.com>
2024-05-20 04:31:24 +0900

    update : フィールド

--- LinkedinLearning/Prestige.Biz/Actor.cs
+++ LinkedinLearning/Prestige.Biz/Actor.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace Prestige.Biz
 {
-    public class Actor
+    public class Actor // 命名はあいまいにしてはならない
     {
+        // propfull と入力することで簡単に記述できる
+        private string jobTitle; // privateはキャメルケース
+
+        public string JobTitle // publicはパスカルケース
+        {
+            get { return jobTitle; }
+            set { jobTitle = value; }
+        }
+
+
+        /// <summary>
+        /// Will return title ※XML doc コメントであればIDEのインテリセンスが効く
+        /// </summary>
+        /// <returns></returns>
         public string GetOccupation()
         {
-            return "Actor";
+            jobTitle = "Actor";
+            return jobTitle;
         }
     }
 }
