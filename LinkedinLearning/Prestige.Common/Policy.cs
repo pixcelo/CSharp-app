fed16513e8e91d4b4f566cc7fb810ff932927702
pixcelo <bodomu@gmail.com>
2024-05-20 09:12:56 +0900

    add: シングルトンなクラス

---
+++ LinkedinLearning/Prestige.Common/Policy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestige.Common
+{
+    /// <summary>
+    /// シングルトンなクラス例
+    /// </summary>
+    public class Policy
+    {
+        private static Policy _instance;
+
+        private Policy() { }
+
+        public static Policy Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Policy();
+                }
+                return _instance;
+            }
+        }
+    }
+}
