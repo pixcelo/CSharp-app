358ed035394ed87a8ce09898736458b77cf611f3
pixcelo <bodomu@gmail.com>
2024-05-22 04:50:09 +0900

    add: 文字列のチェックメソッド

---
+++ LinkedinLearning/ConsoleDotNetCore/Basis/StringTips.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNetCore.Basis
+{
+    public static class StringTips
+    {
+        public static string NormalizeString(string input)
+        {
+            return input.ToLower().Trim().Replace(",", "");
+        }
+
+        /// <summary>
+        /// 文字列がすべて大文字かを判定する
+        /// </summary>
+        public static bool IsUppercase(string s)
+        {
+            return s.All(char.IsUpper);
+        }
+
+        /// <summary>
+        /// 文字列がすべて小文字かを判定する
+        /// </summary>
+        public static bool IsLowercase(string s)
+        {
+            return s.All(char.IsLower);
+        }
+
+        /// <summary>
+        /// 文字列が、大文字、小文字、数字のいずれかを
+        /// 少なくとも1つ含む複雑なパスワードかどうかを判定
+        /// </summary>       
+        public static bool IsPasswordComplex(string s)
+        {
+            return s.Any(char.IsUpper) && s.Any(char.IsLower)
+                && s.Any(char.IsDigit);
+        }
+    }
+}
---
+++ LinkedinLearning/ConsoleDotNetCoreTests/Basis/StringTipsTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleDotNetCore.Basis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNetCore.Basis.Tests
+{
+    [TestClass]
+    public class StringTipsTests
+    {
+        [TestMethod]
+        public void TestNormalizeString()
+        {
+            var expected = "hello there buddy";
+
+            var actual = StringTips.NormalizeString(" Hello There, BUDDY  ");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestIsUppercaseReturnTrue()
+        {
+            var actual = StringTips.IsUppercase("HELLO");
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void TestIsUppercaseReturnFalse()
+        {
+            var actual = StringTips.IsUppercase("Hello");
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void TestIsLowercaseReturnTrue()
+        {
+            var actual = StringTips.IsLowercase("hello");
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void TestIsLowercaseReturnFalse()
+        {
+            var actual = StringTips.IsLowercase("Hello");
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void TestIsPasswordComplexReturnTrue()
+        {
+            var actual = StringTips.IsPasswordComplex("Hell0");
+
+            Assert.IsTrue(actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("Hello")]
+        [DataRow("HellO")]
+        [DataRow("HeLlo")]
+        [DataRow("hello")]
+        [DataRow(" ")]
+        public void TestIsPasswordComplexReturnFalse(string input)
+        {
+            var actual = StringTips.IsPasswordComplex(input);
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
