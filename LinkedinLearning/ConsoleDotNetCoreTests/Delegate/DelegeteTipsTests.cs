371b1b177ba429b935cdc6ed5bedc7bf94341f0b
pixcelo <bodomu@gmail.com>
2024-06-05 04:44:19 +0900

    add: delegeteテストの動作確認

---
+++ LinkedinLearning/ConsoleDotNetCoreTests/Delegate/DelegeteTipsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleDotNetCore.Delegate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleDotNetCore.Basis;
+
+namespace ConsoleDotNetCore.Delegate.Tests
+{
+    [TestClass]
+    public class DelegeteTipsTests
+    {
+        [TestMethod]
+        public void DeletegeFuncTest()
+        {
+            var delegeteTips = new DelegeteTips();
+            var expected = "DeletegeFunc";
+
+            Func<string> actual = delegeteTips.DeletegeFunc;
+
+            Assert.AreEqual(expected, actual());
+        }
+    }
+}
