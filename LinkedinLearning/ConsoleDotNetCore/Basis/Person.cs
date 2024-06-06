69986348958e8bb3fd2443e954c638fdbdf2e9aa
pixcelo <bodomu@gmail.com>
2024-06-07 06:25:03 +0900

    add: コンストラクタの動きを確認

---
+++ LinkedinLearning/ConsoleDotNetCore/Basis/Person.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleDotNetCore.DesignPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+namespace ConsoleDotNetCore.Basis
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        /// <summary>
+        /// 名前のみを受け取るコンストラクタ
+        /// </summary>
+        /// <param name="name"></param>
+        public Person(string name) : this(name, 0) { }
+        
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public Person() : this("Unknown", 0) { }
+    }
+}
--- LinkedinLearning/ConsoleDotNetCore/Program.cs
+++ LinkedinLearning/ConsoleDotNetCore/Program.cs
@@ -10,7 +10,17 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var model = new Iterator();
-        model.Run();
+        //var model = new Iterator();
+        //model.Run();
+
+        // コンストラクタの確認
+        var person = new Person("Taro", 20);
+        Console.WriteLine(person.Name + " " + person.Age);
+
+        var person2 = new Person("Ken");
+        Console.WriteLine(person2.Name + " " + person2.Age);
+
+        var person3 = new Person();
+        Console.WriteLine(person3.Name + " " + person3.Age);
     }
 }
