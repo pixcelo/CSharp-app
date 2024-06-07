b4071906acc4bd8ca2c751a68c2a6cd80b4ca613
pixcelo <bodomu@gmail.com>
2024-06-08 05:22:58 +0900

    add: ドメイン駆動開発

---
+++ DDD/DDD.WinForm/WeatherLatestView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDD.WinForm
+{
+    public partial class WeatherLatestView : Form
+    {
+        public WeatherLatestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
