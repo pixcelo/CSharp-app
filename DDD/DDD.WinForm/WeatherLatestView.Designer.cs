972e1d4fcf7e5850cc5b96d601416462eb1d8d0f
pixcelo <bodomu@gmail.com>
2024-06-08 06:28:35 +0900

    add: SQLiteからデータを取得して画面に表示する

--- DDD/DDD.WinForm/WeatherLatestView.Designer.cs
+++ DDD/DDD.WinForm/WeatherLatestView.Designer.cs
@@ -28,20 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DataDateLabel = new System.Windows.Forms.Label();
+            this.ConditionLabel = new System.Windows.Forms.Label();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.AreaIdTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地域";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "日時";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "状態";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "温度";
+            // 
+            // DataDateLabel
+            // 
+            this.DataDateLabel.AutoSize = true;
+            this.DataDateLabel.Location = new System.Drawing.Point(78, 89);
+            this.DataDateLabel.Name = "DataDateLabel";
+            this.DataDateLabel.Size = new System.Drawing.Size(35, 12);
+            this.DataDateLabel.TabIndex = 8;
+            this.DataDateLabel.Text = "label5";
+            // 
+            // ConditionLabel
+            // 
+            this.ConditionLabel.AutoSize = true;
+            this.ConditionLabel.Location = new System.Drawing.Point(78, 119);
+            this.ConditionLabel.Name = "ConditionLabel";
+            this.ConditionLabel.Size = new System.Drawing.Size(35, 12);
+            this.ConditionLabel.TabIndex = 10;
+            this.ConditionLabel.Text = "label6";
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Location = new System.Drawing.Point(78, 151);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(35, 12);
+            this.TemperatureLabel.TabIndex = 12;
+            this.TemperatureLabel.Text = "label7";
+            // 
+            // AreaIdTextBox
+            // 
+            this.AreaIdTextBox.Location = new System.Drawing.Point(80, 51);
+            this.AreaIdTextBox.Name = "AreaIdTextBox";
+            this.AreaIdTextBox.Size = new System.Drawing.Size(100, 19);
+            this.AreaIdTextBox.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "直近値";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // WeatherLatestView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AreaIdTextBox);
+            this.Controls.Add(this.TemperatureLabel);
+            this.Controls.Add(this.ConditionLabel);
+            this.Controls.Add(this.DataDateLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "WeatherLatestView";
             this.Text = "WeatherLatestView";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label DataDateLabel;
+        private System.Windows.Forms.Label ConditionLabel;
+        private System.Windows.Forms.Label TemperatureLabel;
+        private System.Windows.Forms.TextBox AreaIdTextBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DDD/DDD.WinForm/WeatherLatestView.cs
+++ DDD/DDD.WinForm/WeatherLatestView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,49 @@ namespace DDD.WinForm
 {
     public partial class WeatherLatestView : Form
     {
+        private readonly string ConnectionString = @"Data Source=C:\Users\bodom\Documents\csharp\DDD\DDD.db;Version=3;";
+
         public WeatherLatestView()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = @"
+                SELECT DataDate, Condition, Temperature
+                FROM Weather
+                WHERE AreaId = @AreaId
+                ORDER BY DataDate DESC
+                LIMIT 1
+                ";
+
+            var dataTable = new DataTable();
+            using (var connection = new SQLiteConnection(this.ConnectionString))
+            using (var command = new SQLiteCommand(sql, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@AreaId", this.AreaIdTextBox.Text);
+                using (var adapter = new SQLiteDataAdapter(command))
+                {
+                    adapter.Fill(dataTable);
+                }
+
+                if (dataTable.Rows.Count == 0)
+                {
+                    this.DataDateLabel.Text = "データなし";
+                    this.ConditionLabel.Text = string.Empty;
+                    this.TemperatureLabel.Text = string.Empty;
+                    return;
+                }
+
+                this.DataDateLabel.Text = dataTable.Rows[0]["DataDate"].ToString();
+                this.ConditionLabel.Text = dataTable.Rows[0]["Condition"].ToString();
+                // 小数点以下2桁で表示する
+                this.TemperatureLabel.Text = 
+                    Math.Round(Convert.ToSingle(dataTable.Rows[0]["Temperature"]), 2) + "℃";
+            }
         }
     }
 }
