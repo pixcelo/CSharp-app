7310abf13e3d62518021f76f1a13fe2473097d9f
pixcelo <bodomu@gmail.com>
2024-06-08 12:09:37 +0900

    update: ユニットテストができるように修正

---
+++ DDD/DDDTest.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DDDTest.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DDDTest.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9248be1-07b8-43cd-a5ee-4515215e68b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DDD/DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using DDD.WinForm.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherLatestViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+            var viewModel = new WeatherLatestViewModel();
+
+            Assert.AreEqual("", viewModel.AreaIdText);
+            Assert.AreEqual("", viewModel.DataDateText);
+            Assert.AreEqual("", viewModel.ConditionText);
+            Assert.AreEqual("", viewModel.TemperatureText);
+        }
+    }
+}
