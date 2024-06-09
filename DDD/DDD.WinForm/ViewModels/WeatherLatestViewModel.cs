7b4dee83f7dcfa821f083ff12299cff4bc2a3bb1
pixcelo <bodomu@gmail.com>
2024-06-10 04:49:00 +0900

    add: 自作モックによるテスト実装（ツール未使用）

--- DDD/DDD.WinForm/ViewModels/WeatherLatestViewModel.cs
+++ DDD/DDD.WinForm/ViewModels/WeatherLatestViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DDD.Domain.Repositoriers;
+using DDD.WinForm.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
@@ -9,9 +11,43 @@ namespace DDD.WinForm.ViewModels
 {
     public class WeatherLatestViewModel
     {
+        private IWeatherRepository weather;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="weather"></param>
+        public WeatherLatestViewModel(IWeatherRepository weather)
+        {
+            this.weather = weather;
+        }
+
         public string AreaIdText { get; set; } = string.Empty;
         public string DataDateText { get; set; } = string.Empty;
         public string ConditionText { get; set; } = string.Empty;
         public string TemperatureText { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 直近の情報を取得する
+        /// </summary>
+        public void Search()
+        {
+            var dataTable = this.weather.GetLatest(Convert.ToInt32(this.AreaIdText));
+
+            if (dataTable.Rows.Count == 0)
+            {
+                this.DataDateText = "データなし";
+                this.ConditionText = string.Empty;
+                this.TemperatureText = string.Empty;
+                return;
+            }
+
+            this.DataDateText = dataTable.Rows[0]["DataDate"].ToString();
+            this.ConditionText = dataTable.Rows[0]["Condition"].ToString();
+            this.TemperatureText =
+                CommonFunc.RoundString(Convert.ToSingle(dataTable.Rows[0]["Temperature"]),
+                CommonConst.TemperatureDigit) + " "
+                + CommonConst.TemperatureUnitName;
+        }
     }
 }
--- DDD/DDDTest.Tests/WeatherLatestViewModelTest.cs
+++ DDD/DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -1,5 +1,8 @@
-﻿using DDD.WinForm.ViewModels;
+﻿using DDD.Domain.Repositoriers;
+using DDD.WinForm.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
 
 namespace DDDTest.Tests
 {
@@ -9,12 +12,46 @@ namespace DDDTest.Tests
         [TestMethod]
         public void シナリオ()
         {
-            var viewModel = new WeatherLatestViewModel();
+            var viewModel = new WeatherLatestViewModel(new WeatherMock());
 
+            // 初期状態
             Assert.AreEqual("", viewModel.AreaIdText);
             Assert.AreEqual("", viewModel.DataDateText);
             Assert.AreEqual("", viewModel.ConditionText);
             Assert.AreEqual("", viewModel.TemperatureText);
+
+            // 情報の取得
+            viewModel.AreaIdText = "1";
+            viewModel.Search();
+            Assert.AreEqual("1", viewModel.AreaIdText);
+            Assert.AreEqual("2024/06/10 4:39:10", viewModel.DataDateText);
+            Assert.AreEqual("2", viewModel.ConditionText);
+            Assert.AreEqual("12.30 ℃", viewModel.TemperatureText);
         }
     }
+
+    /// <summary>
+    /// Weatherのモック
+    /// </summary>
+    internal class WeatherMock : IWeatherRepository
+    {
+        public DataTable GetLatest(int areaId)
+        {
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("AreaId", typeof(int));
+            dataTable.Columns.Add("DataDate", typeof(DateTime));
+            dataTable.Columns.Add("Condition", typeof(int));
+            dataTable.Columns.Add("Temperature", typeof(float));
+
+            var newRow = dataTable.NewRow();
+            newRow["AreaId"] = 1;
+            newRow["DataDate"] = Convert.ToDateTime("2024/06/10 4:39:10");
+            newRow["Condition"] = 2;
+            newRow["Temperature"] = 12.3f;
+
+            dataTable.Rows.Add(newRow);
+            return dataTable;
+        }
+    }
+
 }
