1c5a1b50f58c68fe27635b79a862cfa2d1ec8f87
pixcelo <bodomu@gmail.com>
2024-06-11 04:48:05 +0900

    add; Weatherのエンティティを追加

---
+++ DDD/DDD.Domain/Entities/WeatherEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DDD.Domain.Entities
+{
+    /// <summary>
+    /// 天気エンティティ
+    /// </summary>
+    public sealed class WeatherEntity
+    {
+        /// <summary>
+        /// 完全コンストラクタパターン
+        /// </summary>
+        /// <param name="areaId"></param>
+        /// <param name="dataDate"></param>
+        /// <param name="condition"></param>
+        /// <param name="temperature"></param>
+        public WeatherEntity(
+            int areaId,
+            DateTime dataDate,
+            int condition,
+            float temperature)
+        {            
+            this.AreaId = areaId;
+            this.DataDate = dataDate;
+            this.Condition = condition;
+            this.Temperature = temperature;
+        }
+
+        // プロパティはインスタンス生成時に設定された値を変更できないようにする
+        public int AreaId { get; }        
+        public DateTime DataDate { get; }
+        public int Condition { get; }
+        public float Temperature { get; }
+    }
+}
