c2a82f895c7dbd3e5523d818a4abfe925b38f852
pixcelo <bodomu@gmail.com>
2024-06-12 04:31:59 +0900

    add: 値オブジェクトをテスト駆動で追加

---
+++ DDD/DDD.Domain/CommonFunc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DDD.Domain
+{
+    /// <summary>
+    /// 共通の機能クラス
+    /// </summary>
+    public static class CommonFunc
+    {
+        /// <summary>
+        /// 小数点N桁までの文字列に変換
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="digit"></param>
+        /// <returns></returns>
+        public static string RoundString(float value, int digit)
+        {
+            var temp = Convert.ToSingle(Math.Round(value, digit));
+            return temp.ToString($"F{digit}");
+        }
+    }
+}
---
+++ DDD/DDD.Domain/ValueObjects/Temperature.cs
@@ -0,0 +1,23 @@
+﻿namespace DDD.Domain.ValueObjects
+{
+    public sealed class Temperature
+    {
+        private string UnitName = "℃";
+        private int Digit = 2;
+
+        public Temperature(float value)
+        {
+            this.Value = value;
+        }
+
+        public float Value { get; }
+        public string DisplayValue
+        {
+            get
+            {
+                return CommonFunc.RoundString(Value, this.Digit)
+                        + " " + this.UnitName;
+            }
+        }
+    }
+}
