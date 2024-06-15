3de87937c5e0b8b7a381ee078968dd43fa571c0e
pixcelo <bodomu@gmail.com>
2024-06-15 16:44:06 +0900

    update: SQLiteHeplerに処理を共通化

--- DDD/DDD.Infrastructure/SQLite/SQLiteHelper.cs
+++ DDD/DDD.Infrastructure/SQLite/SQLiteHelper.cs
@@ -8,7 +8,32 @@ namespace DDD.Infrastructure.SQLite
     {
         internal const string ConnectionString = @"Data Source=C:\Users\bodom\Documents\csharp\DDD\DDD.db;Version=3;";
 
-        internal static IReadOnlyList<T> Query<T>(string sql, Func<SQLiteDataReader, T> CreateEntity)
+        /// <summary>
+        /// 全件取得
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="CreateEntity"></param>
+        /// <returns></returns>
+        internal static IReadOnlyList<T> Query<T>(
+            string sql,
+            Func<SQLiteDataReader, T> CreateEntity)
+        {
+            return Query(sql, null, CreateEntity);
+        }
+
+        /// <summary>
+        /// 全件取得
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <param name="CreateEntity"></param>
+        /// <returns></returns>
+        internal static IReadOnlyList<T> Query<T>(
+            string sql,
+            SQLiteParameter[] parameters, 
+            Func<SQLiteDataReader, T> CreateEntity)
         {
             var result = new List<T>();
 
@@ -16,6 +41,11 @@ namespace DDD.Infrastructure.SQLite
             using (var command = new SQLiteCommand(sql, connection))
             {
                 connection.Open();
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }                
 
                 using (var reader = command.ExecuteReader())
                 {
@@ -27,6 +57,59 @@ namespace DDD.Infrastructure.SQLite
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// 1件のみ取得する nullEntiryはnullを返却するためのもの
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="CreateEntity"></param>
+        /// <param name="nullEntity"></param>
+        /// <returns></returns>
+        internal static T QuerySingle<T>(
+            string sql,            
+            Func<SQLiteDataReader, T> CreateEntity,
+            T nullEntity)
+        {
+            return QuerySingle(sql, null, CreateEntity, nullEntity);
+        }
+
+        /// <summary>
+        /// 1件のみ取得する nullEntiryはnullを返却するためのもの
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <param name="CreateEntity"></param>        
+        /// <param name="nullEntity"></param>
+        /// <returns></returns>
+        internal static T QuerySingle<T>(
+            string sql,
+            SQLiteParameter[] parameters,
+            Func<SQLiteDataReader, T> CreateEntity,            
+            T nullEntity)
+        {
+            using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+            using (var command = new SQLiteCommand(sql, connection))
+            {
+                connection.Open();
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return CreateEntity(reader);
+                    }
+                }
+            }
+
+            return nullEntity;
         }
     }
 }
--- DDD/DDD.Infrastructure/SQLite/WeatherSQLite.cs
+++ DDD/DDD.Infrastructure/SQLite/WeatherSQLite.cs
@@ -2,13 +2,14 @@
 using DDD.Domain.Repositoriers;
 using DDD.Infrastructure.SQLite;
 using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Data.SQLite;
 
 namespace DDD.Infrastracture.SQLite
 {
     public class WeatherSQLite : IWeatherRepository
-    {        
-
+    {
         public WeatherEntity GetLatest(int areaId)
         {
             string sql = @"
@@ -18,27 +19,54 @@ namespace DDD.Infrastracture.SQLite
                 ORDER BY DataDate DESC
                 LIMIT 1
                 ";
-            
-            using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
-            using (var command = new SQLiteCommand(sql, connection))
-            {
-                connection.Open();
 
-                command.Parameters.AddWithValue("@AreaId", areaId);
-                using (var reader = command.ExecuteReader())
+            return SQLiteHelper.QuerySingle(
+                sql,
+                new List<SQLiteParameter>
                 {
-                    while (reader.Read())
-                    {
-                        return new WeatherEntity(
-                            areaId,
-                            Convert.ToDateTime(reader["DataDate"]),
-                            Convert.ToInt32(reader["Condition"]),
-                            Convert.ToSingle(reader["Temperature"]));
-                    }
-                }
-            }
-
-            return null;
+                    new SQLiteParameter("@AreaId", areaId),
+                }.ToArray(),
+                reader =>
+                {
+                    return new WeatherEntity(
+                        areaId,
+                        Convert.ToDateTime(reader["DataDate"]),
+                        Convert.ToInt32(reader["Condition"]),
+                        Convert.ToSingle(reader["Temperature"]));
+                },
+                null);
         }
+
+        //public WeatherEntity GetLatest(int areaId)
+        //{
+        //    string sql = @"
+        //        SELECT DataDate, Condition, Temperature
+        //        FROM Weather
+        //        WHERE AreaId = @AreaId
+        //        ORDER BY DataDate DESC
+        //        LIMIT 1
+        //        ";
+            
+        //    using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+        //    using (var command = new SQLiteCommand(sql, connection))
+        //    {
+        //        connection.Open();
+
+        //        command.Parameters.AddWithValue("@AreaId", areaId);
+        //        using (var reader = command.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                return new WeatherEntity(
+        //                    areaId,
+        //                    Convert.ToDateTime(reader["DataDate"]),
+        //                    Convert.ToInt32(reader["Condition"]),
+        //                    Convert.ToSingle(reader["Temperature"]));
+        //            }
+        //        }
+        //    }
+
+        //    return null;
+        //}
     }
 }
