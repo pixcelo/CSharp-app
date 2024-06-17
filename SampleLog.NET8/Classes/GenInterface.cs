85157a142fd49642d0e44ab1cbd63ef80be50b71
pixcelo <bodomu@gmail.com>
2024-06-18 06:07:25 +0900

    add: インターフェイス制約

---
+++ SampleLog.NET8/Classes/GenInterface.cs
@@ -0,0 +1,42 @@
+﻿namespace SampleLog.NET8.Classes
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+
+    public class Product : IEntity, IDisposable
+    {
+        public int Id { get; set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// インターフェイスの制約を持つジェネリッククラス
+    /// インターフェイスの制約は複数指定できる
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenInterface<T> where T : IEntity, IDisposable
+    {
+        private List<T> entities = new List<T>();
+
+        public T? Find(int id)
+        {
+            // インターフェイスの制約があることでインターフェイスのプロパティを使用できる
+            return this.entities.Find(x => x.Id == id);
+        }
+    }
+
+    public class ClientD
+    {
+        public static void Use()
+        {
+            // インターフェイスを強制する制約
+            var product = new GenInterface<Product>();
+        }
+    }
+}
