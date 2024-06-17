2ec77b2435ded47dc94ac71c13461179f6efbc59
pixcelo <bodomu@gmail.com>
2024-06-18 05:33:31 +0900

    add

---
+++ SampleLog.NET8/Classes/GenForm.cs
@@ -0,0 +1,34 @@
+﻿using SampleLog.NET8.Forms;
+
+namespace SampleLog.NET8.Classes
+{
+    /// <summary>
+    /// Formクラスのジェネリッククラス
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenForm<T> where T : Form
+    {
+        public string Key { get; set; }
+        public T Value { get; set; }
+
+        public void Run()
+        {
+            // 制約で定義した基底クラスのメソッドを呼び出す
+            this.Value.ShowDialog();
+        }
+    }
+
+    /// <summary>
+    /// ジェネリッククラスを使用するクライアントクラス
+    /// </summary>
+    public class  ClientA
+    {
+        public static void Use()
+        {
+            var form = new GenForm<Form>(); // OK
+            var form2 = new GenForm<SubForm>(); // OK
+            //var form3 = new GenForm<string>(); // コンパイルエラー
+        }
+    }
+
+}
