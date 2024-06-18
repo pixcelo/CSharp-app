6d1693ee1b6333ef0cf8327e6ba9c85078833ac4
pixcelo <bodomu@gmail.com>
2024-06-19 03:21:28 +0900

    update: notnull制約

--- SampleLog.NET8/Classes/KeyValue.cs
+++ SampleLog.NET8/Classes/KeyValue.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SampleLog.NET8.Classes
+﻿namespace SampleLog.NET8.Classes
 {
     /// <summary>
     /// ジェネリッククラス
     /// </summary>
     /// <see href="https://learn.microsoft.com/ja-jp/dotnet/api/system.collections.generic?view=net-8.0"/>
     /// <typeparam name="T"></typeparam>
-    public class KeyValue<T>
+    public class KeyValue<T> where T : notnull
     {
         public string Key { get; set; }
         public T Value { get; set; }
+    }
+
+    public class ClientKeyValue
+    {
+        public static void Use()
+        {
+            var a = new KeyValue<int>();
+            var b = new KeyValue<string>();
+
+            // notnull制約をつけると、null許容コンテキストが有効の場合のみ警告がでる（コンパイルは通る）
+            // https://learn.microsoft.com/ja-jp/dotnet/csharp/language-reference/keywords/where-generic-type-constraint
+            // https://learn.microsoft.com/ja-jp/dotnet/csharp/nullable-references#nullable-contexts
+            var c = new KeyValue<int?>();
+            var d = new KeyValue<string?>();
+        }
+
     }
 }
