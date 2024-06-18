85110cc416bd96f76402a01286084f5acc465066
pixcelo <bodomu@gmail.com>
2024-06-19 03:14:15 +0900

    update

--- SampleLog.NET8/Classes/GenInterface.cs
+++ SampleLog.NET8/Classes/GenInterface.cs
@@ -39,4 +39,31 @@
             var product = new GenInterface<Product>();
         }
     }
+
+    /// <summary>
+    /// コンストラクタ制約
+    /// 使う側のクラスに空のコンストラクタが必要（またはコンストラクタなし）
+    /// </summary>
+    /// <see href="https://learn.microsoft.com/ja-jp/dotnet/csharp/language-reference/keywords/new-constraint"/>
+    /// <typeparam name="T"></typeparam>
+    public class ClassA<T> where T : IEntity, new()
+    {
+        private T Value = new T();
+        private List<T> entities = new List<T>();
+
+        public T? Find(int id)
+        {
+            var result = this.entities.Find(x => x.Id == id);
+
+            // コンストラクタ制約によって、値がnullの場合は
+            // デフォルトのインスタンスを返すことができる
+            if (result == null)
+            {
+                return new T();
+            }
+
+            return result;
+        }
+    }
+
 }
