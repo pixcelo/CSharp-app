b989995bb570ef148546d1a771ab1693e77cd8c5
tetsu.k <bodomu@gmail.com>
2024-10-14 09:35:17 +0900

    [add] 金額クラスにシステム設定を注入する案のたたき

---
+++ LinkedinLearning/ConsoleDotNetFramework/Classes/Price.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleDotNetFramework.Classes
+{
+    /// <summary>
+    /// 金額クラス
+    /// </summary>
+    public sealed class Price
+    {
+        /// <summary>
+        /// システム設定
+        /// </summary>
+        private readonly ISystemSettings systemSettings;
+
+        /// <summary>
+        /// 値 ※データストアに保存する値
+        /// </summary>
+        public readonly decimal value;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Price(
+            ISystemSettings systemSettings,
+            decimal value)
+        {
+            this.systemSettings = systemSettings;
+            this.value = value;
+        }        
+
+        public string DisplayValue()
+        {
+            var priceSettings = (PriceSystemSettings)this.systemSettings;
+            return Math.Round(this.value, priceSettings.DecimalPlaces).ToString($"F{priceSettings.DecimalPlaces}");
+        }
+    }
+}
---
+++ LinkedinLearning/ConsoleDotNetFramework/Classes/SystemSettings.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleDotNetFramework.Classes
+{
+    public interface ISystemSettings
+    {
+    }
+
+    public sealed class PriceSystemSettings : ISystemSettings
+    {
+        private readonly int setting1;
+        private readonly int setting2;
+        private readonly bool setting3;
+        private readonly int decimalPlaces;
+
+        public PriceSystemSettings(
+            int setting1,
+            int setting2,
+            bool setting3,
+            int decimalPlaces)
+        {
+            this.setting1 = setting1;
+            this.setting2 = setting2;
+            this.setting3 = setting3;
+            this.decimalPlaces = decimalPlaces;
+        }
+
+        public int Setting1 { get { return this.setting1; } }
+        public int Setting2 { get { return this.setting2; } }
+        public bool Setting3 { get { return this.setting3; } }
+        public int DecimalPlaces { get { return this.decimalPlaces; } }
+    }
+}
