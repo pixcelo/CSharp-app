0717c37338bb01efffbda26b9eebf53866196fb8
tetsu.k <bodomu@gmail.com>
2025-01-18 06:54:47 +0900

    [mod] ファサードのAPI名を揃える

--- DesignPattern/Machine/Machine/MachineFacade.cs
+++ DesignPattern/Machine/Machine/MachineFacade.cs
@@ -14,9 +14,37 @@ namespace Machine
     /// </summary>
     public static class MachineFacade
     {
+        private static int _fanStopValue;
+
+        /// <summary>
+        /// 同じようなAPIは、語尾を変えてクライアント側に提供する
+        /// </summary>
+        /// <returns></returns>
+        public static int BoxInternalTemperatureFunStop()
+        {
+            FanStop(0);
+
+            try
+            {
+                System.Threading.Thread.Sleep(1000);
+                var result = BoxGetInternalTemperature();
+                _fanStopValue = result;
+                return result;
+            }
+            finally
+            {
+                FanStart(0);
+            }
+        }
+
         public static int BoxGetInternalTemperature()
         {
             return new Box().GetInternalTemperature();
+        }
+
+        public static int BoxGetInternalTemperatureInMemory()
+        {
+            return _fanStopValue;
         }
 
         public static int BoxGetExternalTemperature()
@@ -26,6 +54,13 @@ namespace Machine
 
         public static void CameraTake()
         {
+            // メソッドに利用手順がある場合にもファサードで吸収する
+            // クライアントは利用手順を知らなくてもよい
+            if (BoxGetInternalTemperature() > 60)
+            {
+                throw new Exception("内部温度が高すぎます");
+            }
+
             new Camera().Take();
         }
 
