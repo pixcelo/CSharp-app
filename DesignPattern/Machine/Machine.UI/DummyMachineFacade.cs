0d7879c8a4acfe9b56b4c83b7cd0e5ddaa33bc57
tetsu.k <bodomu@gmail.com>
2025-01-18 07:27:58 +0900

    [add] ダミーのファサード（実装する人が別の場合に一旦ダミーで実装しておく。テスト用）

---
+++ DesignPattern/Machine/Machine.UI/DummyMachineFacade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Machine.UI
+{
+    internal sealed class DummyMachineFacade : IMachineFacade
+    {
+        public void BacklightOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BoxGetExternalTemperature()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BoxGetInternalTemperature()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BoxGetInternalTemperatureInMemory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BoxInternalTemperatureFunStop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CameraTake()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FanEntity FanSpin(int fanId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FanStart(int fanId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FanStop(int fanId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PowerOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
